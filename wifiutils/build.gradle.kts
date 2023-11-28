5853e4a644833fa59c2eea59a8264d70fa9cd6cf
ducanh <ducanh@olli-ai.com>
2023-11-28 16:06:43 +0700

    fix build release.

--- wifiutils/build.gradle.kts
+++ wifiutils/build.gradle.kts
@@ -53,110 +53,110 @@ dependencies {
 }
 
 
-val dokkaTask by tasks.creating(org.jetbrains.dokka.gradle.DokkaTask::class) {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    description = "Assembles Kotlin docs with Dokka"
-    outputFormat = "javadoc"
-    outputDirectory = "$buildDir/dokka"
-}
-
-val dokkaJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("dokka")
-    from("$buildDir/dokka")
-    dependsOn(dokkaTask)
-}
-
-val sourcesJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("sources")
-    from(android.sourceSets.getByName("main").java.srcDirs)
-}
-
-val artifactDir = "$buildDir/outputs/aar/${project.name}-release.aar"
-
-publishing {
-    publications {
-        create<MavenPublication>(Artifact.ARTIFACT_NAME) {
-            //from(components["java"]) kinda broken on android
-            groupId = Artifact.ARTIFACT_GROUP
-            artifactId = Artifact.ARTIFACT_NAME
-            version = Artifact.VERSION_NAME
-            artifacts {
-                artifact(sourcesJar)
-                artifact(dokkaJar)
-                artifact(artifactDir)
-            }
-
-            pom.withXml {
-                asNode().apply {
-                    appendNode("description", Artifact.POM_DESC)
-                    appendNode("name", Artifact.LIBRARY_NAME)
-                    appendNode("url", Artifact.POM_URL)
-                    appendNode("licenses").appendNode("license").apply {
-                        appendNode("name", Artifact.POM_LICENSE_NAME)
-                        appendNode("url", Artifact.POM_LICENSE_URL)
-                        appendNode("distribution", Artifact.POM_LICENSE_DIST)
-                    }
-                    appendNode("developers").appendNode("developer").apply {
-                        appendNode("id", Artifact.POM_DEVELOPER_ID)
-                        appendNode("name", Artifact.POM_DEVELOPER_NAME)
-                    }
-                    appendNode("scm").apply {
-                        appendNode("url", Artifact.POM_SCM_URL)
-                    }
-                }
-                val dependenciesNode = asNode().appendNode("dependencies")
-                val configurationNames = arrayOf("implementation")
-                configurationNames.forEach { configurationName ->
-                    configurations[configurationName].allDependencies.distinct().forEach {
-                        if (it.group != null && it.name != null) {
-                            val dependencyNode = dependenciesNode.appendNode("dependency")
-                            dependencyNode.appendNode("groupId", it.group)
-                            dependencyNode.appendNode("artifactId", it.name)
-                            dependencyNode.appendNode("version", it.version)
-                            dependencyNode.appendNode("scope", "runtime")
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-bintray {
-    val properties = Properties()
-    properties.load(project.rootProject.file("local.properties").inputStream())
-
-    // Getting bintray user and key from properties file or command line
-    user =
-        if (properties.hasProperty("bintrayUser")) properties.getProperty("bintrayUser") as String else "thanosfisherman"
-    key =
-        if (properties.hasProperty("bintrayKey")) properties.getProperty("bintrayKey") as String else ""
-
-    // Automatic publication enabled
-    publish = true
-    dryRun = false
-
-    // Set maven publication onto bintray plugin
-    setPublications(Artifact.ARTIFACT_NAME)
-
-    // Configure package
-    pkg.apply {
-        repo = "maven"
-        name = Artifact.BINTRAY_NAME
-        setLicenses("Apache-2.0")
-        setLabels("Kotlin", "android", "wifi", "utils")
-        vcsUrl = Artifact.POM_SCM_URL
-        websiteUrl = Artifact.POM_URL
-        issueTrackerUrl = Artifact.POM_ISSUE_URL
-        githubRepo = Artifact.GITHUB_REPO
-        githubReleaseNotesFile = Artifact.GITHUB_README
-
-        // Configure version
-        version.apply {
-            name = Artifact.VERSION_NAME
-            desc = Artifact.POM_DESC
-            released = Date().toString()
-            vcsTag = Artifact.VERSION_NAME
-        }
-    }
-}
+//val dokkaTask by tasks.creating(org.jetbrains.dokka.gradle.DokkaTask::class) {
+//    group = JavaBasePlugin.DOCUMENTATION_GROUP
+//    description = "Assembles Kotlin docs with Dokka"
+//    outputFormat = "javadoc"
+//    outputDirectory = "$buildDir/dokka"
+//}
+//
+//val dokkaJar by tasks.creating(Jar::class) {
+//    archiveClassifier.set("dokka")
+//    from("$buildDir/dokka")
+//    dependsOn(dokkaTask)
+//}
+//
+//val sourcesJar by tasks.creating(Jar::class) {
+//    archiveClassifier.set("sources")
+//    from(android.sourceSets.getByName("main").java.srcDirs)
+//}
+//
+//val artifactDir = "$buildDir/outputs/aar/${project.name}-release.aar"
+//
+//publishing {
+//    publications {
+//        create<MavenPublication>(Artifact.ARTIFACT_NAME) {
+//            //from(components["java"]) kinda broken on android
+//            groupId = Artifact.ARTIFACT_GROUP
+//            artifactId = Artifact.ARTIFACT_NAME
+//            version = Artifact.VERSION_NAME
+//            artifacts {
+//                artifact(sourcesJar)
+//                artifact(dokkaJar)
+//                artifact(artifactDir)
+//            }
+//
+//            pom.withXml {
+//                asNode().apply {
+//                    appendNode("description", Artifact.POM_DESC)
+//                    appendNode("name", Artifact.LIBRARY_NAME)
+//                    appendNode("url", Artifact.POM_URL)
+//                    appendNode("licenses").appendNode("license").apply {
+//                        appendNode("name", Artifact.POM_LICENSE_NAME)
+//                        appendNode("url", Artifact.POM_LICENSE_URL)
+//                        appendNode("distribution", Artifact.POM_LICENSE_DIST)
+//                    }
+//                    appendNode("developers").appendNode("developer").apply {
+//                        appendNode("id", Artifact.POM_DEVELOPER_ID)
+//                        appendNode("name", Artifact.POM_DEVELOPER_NAME)
+//                    }
+//                    appendNode("scm").apply {
+//                        appendNode("url", Artifact.POM_SCM_URL)
+//                    }
+//                }
+//                val dependenciesNode = asNode().appendNode("dependencies")
+//                val configurationNames = arrayOf("implementation")
+//                configurationNames.forEach { configurationName ->
+//                    configurations[configurationName].allDependencies.distinct().forEach {
+//                        if (it.group != null && it.name != null) {
+//                            val dependencyNode = dependenciesNode.appendNode("dependency")
+//                            dependencyNode.appendNode("groupId", it.group)
+//                            dependencyNode.appendNode("artifactId", it.name)
+//                            dependencyNode.appendNode("version", it.version)
+//                            dependencyNode.appendNode("scope", "runtime")
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//bintray {
+//    val properties = Properties()
+//    properties.load(project.rootProject.file("local.properties").inputStream())
+//
+//    // Getting bintray user and key from properties file or command line
+//    user =
+//        if (properties.hasProperty("bintrayUser")) properties.getProperty("bintrayUser") as String else "thanosfisherman"
+//    key =
+//        if (properties.hasProperty("bintrayKey")) properties.getProperty("bintrayKey") as String else ""
+//
+//    // Automatic publication enabled
+//    publish = true
+//    dryRun = false
+//
+//    // Set maven publication onto bintray plugin
+//    setPublications(Artifact.ARTIFACT_NAME)
+//
+//    // Configure package
+//    pkg.apply {
+//        repo = "maven"
+//        name = Artifact.BINTRAY_NAME
+//        setLicenses("Apache-2.0")
+//        setLabels("Kotlin", "android", "wifi", "utils")
+//        vcsUrl = Artifact.POM_SCM_URL
+//        websiteUrl = Artifact.POM_URL
+//        issueTrackerUrl = Artifact.POM_ISSUE_URL
+//        githubRepo = Artifact.GITHUB_REPO
+//        githubReleaseNotesFile = Artifact.GITHUB_README
+//
+//        // Configure version
+//        version.apply {
+//            name = Artifact.VERSION_NAME
+//            desc = Artifact.POM_DESC
+//            released = Date().toString()
+//            vcsTag = Artifact.VERSION_NAME
+//        }
+//    }
+//}
